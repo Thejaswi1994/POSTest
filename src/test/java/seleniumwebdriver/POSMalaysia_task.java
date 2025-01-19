908a98e9df90cc5bb8ee7923c0484d57bcdb4981
Thejaswi1994 <thejaswireddy01@gmail.com>
2025-01-19 00:14:36 -0500

    Initial Commit

---
+++ src/test/java/seleniumwebdriver/POSMalaysia_task.java
@@ -0,0 +1,104 @@
+package seleniumwebdriver;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+
+public class POSMalaysia_task {
+	public static void main(String[] args) {
+	
+	WebDriver driver=new ChromeDriver();
+	
+	driver.get("https://www.pos.com.my/send/ratecalculator");
+	
+	driver.manage().window().maximize();
+	
+	WebElement postcodeInput = driver.findElement(By.xpath("//input[@formcontrolname='postcodeFrom']"));
+    postcodeInput.sendKeys("35600");
+    
+    By inputBoxLocator = By.xpath("//input[@placeholder='Select country']");
+  
+    By optionLocator = By.xpath("//small[@title=\"India - IN\"]");
+
+    try {
+     
+        WebElement inputBox = driver.findElement(inputBoxLocator);
+        inputBox.clear();
+        inputBox.sendKeys("India");
+
+      
+        Thread.sleep(3000);
+        WebElement indiaOption = driver.findElement(optionLocator);
+        indiaOption.click();
+      
+    
+    WebElement Weight= driver.findElement(By.xpath("//input[@formcontrolname='itemWeight']"));
+  Weight.sendKeys("1");
+   
+    
+WebElement Calculatebutton = driver.findElement(By.xpath("//div/a[text()=' Calculate ']"));
+   
+   Calculatebutton.click();
+   Thread.sleep(3000);
+    
+   By locator = By.xpath("//p[text()='Select a quote to start booking your shipment.']");
+   String expectedText = "Select a quote to start booking your shipment.";
+
+  
+       WebElement element = driver.findElement(locator);
+
+       String actualText = element.getText();
+
+       Assert.assertEquals(actualText, expectedText, "Expect the text to match: " + expectedText);
+
+   } catch (Exception e) {
+     
+       Assert.fail("Test failed due to exception: " + e.getMessage());
+   } finally {
+      
+       driver.quit();
+   }
+}
+	}
+
+/*test case
+ Launch browser
+ Open URL
+ Validate title should be "Your store"
+ Close browser
+ */
+
+public class firstautoscript {
+	public static void main(String[] args) {
+		
+		//1.Launch browser (chrome)
+		//ChromeDriver driver=new ChromeDriver();
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.pos.com.my/send/ratecalculator");
+		
+		//validate the title "Parcel Shipment Rate Calculator | Pos Malaysia
+		
+		String act_title=driver.getTitle();
+		if(act_title.equals("Parcel Shipment Rate Calculator | Pos Malaysia"))
+		{
+			System.out.println("test passed");
+			driver.quit();
+		}
+		else
+		{
+			System.out.println("test failed");
+			driver.quit();
+		}
+
+	}
+	}
